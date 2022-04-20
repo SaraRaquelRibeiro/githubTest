bd24c9323559a9170ad8771a7ea26580f9c2a8de
SaraRaquelRibeiro <lins.ribeiro.001@gmail.com>
2022-04-20 18:15:39 +0100

    Initial Commit

---
+++ githubTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  githubTest
+//
+//  Created by Sara Ribeiro on 20/04/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ githubTest/githubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  githubTestApp.swift
+//  githubTest
+//
+//  Created by Sara Ribeiro on 20/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct githubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
